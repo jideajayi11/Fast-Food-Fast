76327c276cbca43cd59df1d8f127a1cfe2e128b7
jideajayi11 <jideajayi11@gmail.com>
2018-09-14 04:45:47 -0700

    add test to get an order [#160492315]

--- server/test/order.test.js
+++ server/test/order.test.js
@@ -14,7 +14,32 @@ describe('Order Endpoints', () => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         expect(res.body).to.have.property('status').equal('success');
-        expect(res.body).to.have.property('message').equal('Retrieved all your orders');
+        expect(res.body).to.have.property('message')
+        .equal('Retrieved all your orders');
+        done();
+      });
+  });
+  it('should get an order', (done) => {
+    chai.request(server)
+      .get('/api/v1/orders/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        expect(res.body).to.have.property('status').equal('success');
+        expect(res.body).to.have.property('message')
+        .equal('Order found');
+        expect(res.body).to.have.property('userId')
+        .equal(3);
+        expect(res.body).to.have.property('foodId')
+        .equal(8);
+        expect(res.body).to.have.property('price')
+        .equal(500);
+        expect(res.body).to.have.property('quantity')
+        .equal(4);
+        expect(res.body).to.have.property('status')
+        .equal('pending');
+        expect(res.body).to.have.property('date')
+        .equal('2018-09-01');
         done();
       });
   });
