60b0b477294c8795a26b292fca4e362e0b471ce6
jideajayi11 <jideajayi11@gmail.com>
2018-09-14 05:43:23 -0700

    add route to add order [#160492474]

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -3,6 +3,6 @@ import Order from '../controllers/orderController';
 export default (app) => {
   app.get('/api/v1/orders', Order.getOrders);
   app.get('/api/v1/orders/:id', Order.getOrder);
-  /*app.post('/api/v1/orders', Order.addOrder);
-  app.put('/api/v1/orders/:id', Order.updateOrder);*/
+  app.post('/api/v1/orders', Order.addOrder);
+  /*app.put('/api/v1/orders/:id', Order.updateOrder);*/
 };
