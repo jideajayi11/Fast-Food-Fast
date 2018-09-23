3e0525689ba6fbefc43c4170800e6878d5c824ae
jideajayi11 <jideajayi11@gmail.com>
2018-09-23 08:40:29 -0700

    [Chore #160661738] create tables if not exists

--- server/helpers/db/index.js
+++ server/helpers/db/index.js
@@ -1,12 +1,10 @@
 import { Pool } from 'pg';
-import sqlTest from './test.sql';
-import sqlDev from './development.sql';
+import tables from './tables.sql';
 import env from 'dotenv';
 
 env.config();
 
 let parameters = {};
-let tables;
 if (process.env.NODE_ENV === 'test') {
   parameters = {
 		user: 'postgres',
@@ -15,12 +13,10 @@ if (process.env.NODE_ENV === 'test') {
 		password: '',
 		port: process.env.PORT
 	};
-	tables = sqlTest;
 } else {
 	parameters = {
 		connectionString: process.env.DB_PATH
 	};
-	tables = sqlDev;
 }
 const pool = new Pool(parameters);
 if(pool.connect())
