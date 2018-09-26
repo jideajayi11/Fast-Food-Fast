42c60825933d409ceb6ee39c806afc80ffcd0d65
jideajayi11 <jideajayi11@gmail.com>
2018-09-26 11:29:17 -0700

    [Feature #160679113] add signup admin endpoint

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -6,4 +6,6 @@ export default (app) => {
     Validate.userSignup, Auth.userSignup);
   app.post('/api/v1/auth/login',
     Validate.userSignin, Auth.userSignin);
+    app.post('/api/v1/admin/signup',
+      Validate.adminSignup, Auth.adminSignup);
 };
